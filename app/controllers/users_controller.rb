934532af94684a30e6e48ef527fdcf04ca051a1f
aoyama <syouta.aoyama20@gmail.com>
2020-09-29 16:02:49 +0900

    Add edit and destroy function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def home
   end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.new(user_params)
 
@@ -19,6 +23,23 @@ class UsersController < ApplicationController
 			render '/users/new'
 		end
   end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+		  redirect_to '/users/index'
+	  else
+			render '/users/edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		redirect_to users_index_path
+	end
 
 	private
 		def	user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/home'
 	get '/users/create', to: 'users#new'
-	post 'users/create'
+	post '/users/create'
+	get '/users/:id/edit', to: 'users#edit', as: 'edit_users'
+	patch '/users/:id', to: 'users#update'
+	delete '/users/:id', to: 'users#destroy', as: 'destroy_users'
 
 	root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
